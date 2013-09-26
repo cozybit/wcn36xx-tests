96272383b255ddd98a091a664c4c5934214a366a
Jason Mobarak <jam@cozybit.com>
2013-09-26 12:43:23 -0700

    Test report + script to run iperf on phones

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -29,3 +29,45 @@ function Q () {
                 $* &> /dev/null
         fi
 }
+
+# grab a list of Xperia X phones
+xperiaz=$(
+      adb devices 2>&1 \
+    | sed 's///g'    \
+    | grep 'device'    \
+    | cut -d$'\t' -f1  \
+    | grep '^BX'       )
+
+adbs() {
+    for dev in $xperiaz; do
+        ip=$(gen_ip $dev)
+        cmd=$(echo ${*} | sed "s/@IP@/$ip/")
+        echo adb -s $dev $cmd 1>&2
+        adb -s $dev $cmd | sed 's///g'
+    done
+}
+
+gen_ip()
+{
+    local serial=$1
+
+    ip1=`printf "%d" \'${serial: -3: 1}`
+    ip2=`printf "%d" \'${serial: -2: 1}`
+    ip3=`printf "%d" \'${serial: -1: 1}`
+
+    ip="10.$ip1.$ip2.$ip3"
+    echo $ip
+}
+
+sleep_countdown() {
+    local count=$1
+    local msg=$2
+    local current=0
+    echo -n "$msg"
+    while [[ $current -lt $count ]]; do
+        sleep 1
+        echo -n "."
+        current=$(($current + 1))
+    done
+    echo
+}
